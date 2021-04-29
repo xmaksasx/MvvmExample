5c6733b5eb79ebe65c8443e60eef76f19b2ebca1
XmaksasX <XmaksasX@gmail.com>
2021-04-29 12:37:10 +0300

    тестирование модели представления данных

--- MvvmExample/ViewModels/MainWindowViewModel.cs
+++ MvvmExample/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MvvmExample.ViewModels.Base;
 
 namespace MvvmExample.ViewModels
 {
-	class MainWindowViewModel
+	internal class MainWindowViewModel : ViewModel
 	{
+		#region Заголовок окна 
+
+		private string _title="Анализ данных";
+
+		/// <summary>Заголовок окна</summary>
+		public string Title
+		{
+			get => _title;
+			set => Set(ref _title, value);
+			//set
+			//{
+			//	//if (Equals(_title, value)) return;
+			//	//_title = value;
+			//	//OnPropertyChanged();
+			//}
+		}
+
+		#endregion
 	}
 }
