c02c967fb82fd373c5441e04b115dc2f55992620
XmaksasX <XmaksasX@gmail.com>
2021-04-29 17:56:30 +0300

    приязка статуса

--- MvvmExample/ViewModels/MainWindowViewModel.cs
+++ MvvmExample/ViewModels/MainWindowViewModel.cs
@@ -5,7 +5,7 @@ namespace MvvmExample.ViewModels
 	internal class MainWindowViewModel : ViewModel
 	{
 		#region Заголовок окна 
-
+		/// <summary>Заголовок окна</summary>
 		private string _title="Анализ данных";
 
 		/// <summary>Заголовок окна</summary>
@@ -13,6 +13,25 @@ namespace MvvmExample.ViewModels
 		{
 			get => _title;
 			set => Set(ref _title, value);
+			//set
+			//{
+			//	//if (Equals(_title, value)) return;
+			//	//_title = value;
+			//	//OnPropertyChanged();
+			//}
+		}
+
+		#endregion
+
+		#region Заголовок окна 
+		/// <summary>Статус программы</summary>
+		private string _status = "Готов!";
+
+		/// <summary>Статус программы</summary>
+		public string Status
+		{
+			get => _title;
+			set => Set(ref _status, value);
 			//set
 			//{
 			//	//if (Equals(_title, value)) return;
