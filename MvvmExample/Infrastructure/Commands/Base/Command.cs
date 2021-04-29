fc2b0a1952836776165b627c8ddd6cfa7fc1ef66
XmaksasX <XmaksasX@gmail.com>
2021-04-29 18:01:12 +0300

    Базовый класс команды

--- MvvmExample/Infrastructure/Commands/Base/Command.cs
+++ MvvmExample/Infrastructure/Commands/Base/Command.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MvvmExample.Infrastructure.Commands.Base
 {
-	class Command
+	internal abstract class Command : ICommand
 	{
+		public abstract bool CanExecute(object parameter);
+
+		public abstract void Execute(object parameter);
+	
+
+		public event EventHandler CanExecuteChanged
+		{
+			add => CommandManager.RequerySuggested += value;
+			remove => CommandManager.RequerySuggested -= value;
+
+		}
 	}
 }
