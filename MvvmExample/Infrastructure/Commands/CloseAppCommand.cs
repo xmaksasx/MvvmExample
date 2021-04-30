449687c76aeaaed0423c5c3d3b85e6f632020333
XmaksasX <XmaksasX@gmail.com>
2021-04-30 12:23:45 +0300

    Определение класса отдельной команды

---
+++ MvvmExample/Infrastructure/Commands/CloseAppCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using MvvmExample.Infrastructure.Commands.Base;
+
+namespace MvvmExample.Infrastructure.Commands
+{
+	class CloseAppCommand : Command
+	{
+		public override bool CanExecute(object parameter) => true;
+
+		public override void Execute(object parameter) => Application.Current.Shutdown();
+	}
+}
