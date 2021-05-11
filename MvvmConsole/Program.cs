96f40081e6bcce44e8aff69b2107279a13f02ea3
XmaksasX <XmaksasX@gmail.com>
2021-05-11 17:40:57 +0300

    Запрос к удаленному серверу

--- MvvmConsole/Program.cs
+++ MvvmConsole/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 
 namespace MvvmConsole
 {
 	class Program
 	{
+		private const string DataUrl = @"https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 		static void Main(string[] args)
 		{
+			//WebClient client = new WebClient();
+			var client = new HttpClient();
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+			var result = client.GetAsync(DataUrl).Result;
+			var csv = result.Content.ReadAsStringAsync().Result;
+			Console.ReadLine();
 		}
 	}
 }
